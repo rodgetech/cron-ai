883e71788614a614e43b5dc1155800ab71a1832f
Luis Rodriguez <luisrodge@outlook.com>
2023-01-30 14:44:43 -0600

    Add viewport meta

--- app/head.tsx
+++ app/head.tsx
@@ -7,6 +7,7 @@ export default function Head() {
         name="keywords"
         content="AI, cron, cron expression, cron expression generator"
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   );
 }
