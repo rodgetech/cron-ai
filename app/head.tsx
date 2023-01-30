db09cc16455d13d963e7b7c44e6a6a5a01ce15e6
Luis Rodriguez <luisrodge@outlook.com>
2023-01-30 10:12:19 -0600

    Add meta tags

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Cron AI - Words to cron expression</title>
+      <meta name="description" content="An easy AI cron expression generator" />
+      <meta
+        name="keywords"
+        content="AI, cron, cron expression, cron expression generator"
+      />
+    </>
+  );
+}
