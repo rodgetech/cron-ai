b0eea451b562fc18e6247cfce4e6638ee2fbdb2b
Luis <rodgetech@gmail.com>
2023-12-23 09:48:22 -0600

    upgrade everything

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -1,27 +1,23 @@
-import { Configuration, OpenAIApi } from "openai-edge";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse  } from "ai";
 
-const config = new Configuration({
+
+
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(config);
+
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { prompt } = await req.json();
 
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
     temperature: 0,
-    max_tokens: 150,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-    stop: [":"],
     stream: true,
-    prompt: `
-    Below is text describing a cron expression.
+    messages: [{role: "system", content: `Below is text describing a cron expression.
     Your goal is to:
     - Convert the text to a valid cron expression.
     - The cron expression you generate must match this regular expression: "^((\*|[0-9]|[1-5][0-9]|60) |(\*|[0-9]|[1-5][0-9]|60) |(\*|[0-9]|[1-2][0-9]|3[0-1]) |(\*|[0-9]|[1-9]|[1-2][0-9]|3[0-1]|4[0-6]|5[0-3]) |(\*|[0-9]|[1-9]|1[0-2]))(\*|\/[0-9]|[0-9\-,\/]+) (\*|\/[0-9]|[0-9\-,\/]+) (\*|\/[0-9]|[0-9\-,\/]+) (\*|\/[0-9]|[0-9\-,\/]+) (\*|\/[0-9]|[0-9\-,\/]+)$"
@@ -30,11 +26,7 @@ export async function POST(req: Request) {
     - Text: A cron that runs every hour
     - Cron: 0 * * * *
     - Text: A cron that runs ever 12 hour
-    - Cron: 0 */12 * * *
-    Below is the text:
-    TEXT: A cron that runs ${prompt}
-    YOUR RESPONSE:
-  `,
+    - Cron: 0 */12 * * *`}, {role: "user", content: prompt}]
   });
 
   // Convert the response into a friendly text-stream
